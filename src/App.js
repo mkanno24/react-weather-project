97207683d700d8ce75793dd155525e69b58ce6e7
mkanno24 <79492650+mkanno24@users.noreply.github.com>
2021-09-16 20:54:50 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {""}{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://app.netlify.com/sites/brave-khorana-df5338/overview"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Mika Kanno
         </a>
-      </header>
+        and is {""}
+        <a
+          href="https://github.com/mkanno24/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
